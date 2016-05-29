a9b70a1f9e259bb9154ee7c3fa9d22efbecffbb7
lDionis <ldioniss@gmail.com>
2016-05-29 13:59:57 +0300

    Video Commit

---
+++ app/src/main/java/com/ldionis/trainupapplication/Config.java
@@ -0,0 +1,13 @@
+package com.ldionis.trainupapplication;
+
+/**
+ * Created by PC on 29.05.2016.
+ */
+public class Config {
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyBKO6Yh22o6nvIk2kgJrOrcVPUFB7a75e4";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
--- app/src/main/java/com/ldionis/trainupapplication/ExcerciseDescriptionActivity.java
+++ app/src/main/java/com/ldionis/trainupapplication/ExcerciseDescriptionActivity.java
@@ -1,5 +1,6 @@
 package com.ldionis.trainupapplication;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
@@ -11,15 +12,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.ldionis.trainupapplication.database.DatabaseHelper;
 
-public class ExcerciseDescriptionActivity extends AppCompatActivity {
+public class ExcerciseDescriptionActivity extends AppCompatActivity implements
+        YouTubePlayer.OnInitializedListener{
+    public static final String DEVELOPER_KEY ="AIzaSyBKO6Yh22o6nvIk2kgJrOrcVPUFB7a75e4";
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+    private static final String VIDEO_ID = "fhWaJi1Hsfo";
+    YouTubePlayerFragment myYouTubePlayerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excercise_description);
+
+        myYouTubePlayerFragment = (YouTubePlayerFragment)getFragmentManager()
+                .findFragmentById(R.id.youtubeplayerfragment);
+        myYouTubePlayerFragment.initialize(DEVELOPER_KEY, this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         String excerciseName = getIntent().getStringExtra("ExcerciseName");
@@ -33,6 +50,44 @@ public class ExcerciseDescriptionActivity extends AppCompatActivity {
         animateImage();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+
+
+
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(
+                    "There was an error initializing the YouTubePlayer (%1$s)",
+                    errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo(VIDEO_ID);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(DEVELOPER_KEY, this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView)findViewById(R.id.youtubeplayerfragment);
     }
 
     public void animateImage(){
