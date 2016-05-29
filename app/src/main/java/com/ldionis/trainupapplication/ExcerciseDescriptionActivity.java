698901c1af5c1be353e6913179dfd2711713bebb
lDionis <ldioniss@gmail.com>
2016-05-29 15:17:20 +0300

    G Commit

--- app/src/main/java/com/ldionis/trainupapplication/ExcerciseDescriptionActivity.java
+++ app/src/main/java/com/ldionis/trainupapplication/ExcerciseDescriptionActivity.java
@@ -7,10 +7,12 @@ import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -74,6 +76,8 @@ public class ExcerciseDescriptionActivity extends AppCompatActivity implements
                                         boolean wasRestored) {
         if (!wasRestored) {
             player.cueVideo(VIDEO_ID);
+            NestedScrollView sv = (NestedScrollView)findViewById(R.id.scr);
+            sv.scrollTo(0, sv.getBottom());
         }
     }
 
